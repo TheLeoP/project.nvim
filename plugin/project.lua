dc624c4a424e1401c6f3567b8a321915c454ff5a
TheLeoP <eugenio2305@hotmail.com>
2026-09-25 09:41:06 -0500

    feat: add projects to history on DirChanged

--- plugin/project.lua
+++ plugin/project.lua
@@ -7,5 +7,13 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
   desc = "Write project.nvim history to file before closing Neovim",
 })
 
+vim.api.nvim_create_autocmd("DirChanged", {
+  group = group,
+  callback = function(args)
+    local cwd = args.file
+    require("project").add(cwd)
+  end,
+})
+
 require("project").read_history()
 require("project").start_history_watcher()
